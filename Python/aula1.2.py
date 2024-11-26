9c7f39fda913d66749b1fa1197374acde3690212
mintysalad <uaupietra@gmail.com>
2024-11-26 12:51:46 +0000

    aula1.2.py

--- Python/aula1.2.py
+++ Python/aula1.2.py
@@ -1,3 +1,49 @@
 faturamento = 1200 # Tipo: int -> número inteiro.
 custo = 750.32 # Tipo: float -> número com casas decimais
 lucro = faturamento - custo
+
+margemLucro = lucro/faturamento
+
+print ("Faturamento foi de: ", faturamento)
+print ("O custo foi de: ", custo)
+print ("O lucro foi de: ", lucro)
+print ("A margem de lucro foi de: ", margemLucro)
+
+mensagem = "O faturamento da loja foi de "
+print(type(mensagem))
+print("A variável Faturamento é do tipo", type(faturamento))
+email = "emailqualquer@gmail.com" #tipo string -> texto str
+teve_lucro = True #variável tipo boolean
+print(type(teve_lucro))
+
+#Mod -> resto da divisão
+print(10 % 3)
+
+Tempo_contrato = 170
+tempoAnos = Tempo_contrato / 12
+tempoMeses = Tempo_contrato % 12
+
+print("Tempo em anos:", int (tempoAnos), "anos e", tempoMeses, "meses")
+
+tempoContrato = 250 -110
+tempoAnos = tempoContrato / 12
+tempoMeses = tempoContrato % 12
+
+print("Tempo em anos:", int (tempoAnos), "anos e", tempoMeses, "meses")
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = "Miau", 69
+print(nome)
+print(idade)
+
+nome2, idade2 = " ", 0
+print(type(nome2))
+print(type(idade2))
+estado = True
+
+print("A variável nome2 é do tipo", type(nome2))
+print("A variável idade2 é do tipo", type(idade2))
+print(type(n3))
+print (type(estado))
+print(type(1+2j))
