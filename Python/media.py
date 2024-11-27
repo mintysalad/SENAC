499d2ea55804d5c538f1c1b7b1565aa2e1d31306
mintysalad <uaupietra@gmail.com>
2024-11-27 14:48:01 +0000

    media.py

---
+++ Python/media.py
@@ -0,0 +1,7 @@
+n1 = n2 = m = float
+print("Valor da média entre dois números!")
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
+
+m = (n1 + n2)/2
+print("O valor da média é de: ", m)
