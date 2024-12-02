67e35ea3452aed6e4bce27073630f97807d716d8
mintysalad <uaupietra@gmail.com>
2024-12-02 12:27:35 +0000

    atividades2

--- Python/atividades2.py
+++ Python/atividades2.py
@@ -1,102 +1,102 @@
-# # Média
+# Média
 
-# n1 = n2 = m = float
-# print("Valor da média entre dois números!")
-# n1 = float(input("Digite o primeiro valor: "))
-# n2 = float(input("Digite o segundo valor: "))
+n1 = n2 = m = float
+print("Valor da média entre dois números!")
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
 
-# m = (n1 + n2)/2
-# print("O valor da média é de: ", m)
+m = (n1 + n2)/2
+print("O valor da média é de: ", m)
 
-# # Par ou Ímpar
+# Par ou Ímpar
 
-# n = float(input("Digite um número para saber se é par ou ímpar: "))
-# resto = n % 2
-# if resto == 0:
-#     print("Este número é par.")
-# else:
-#     print("Este número é ímpar.")
+n = float(input("Digite um número para saber se é par ou ímpar: "))
+resto = n % 2
+if resto == 0:
+    print("Este número é par.")
+else:
+    print("Este número é ímpar.")
 
-#     # Celsius pra Farenheit
+    # Celsius pra Farenheit
 
-# c = float(input('Digite a temperatura em °c: '))
-# f = float((9 * c) / 5) + 32
-# print('A temperatura em fahrenheit: {0}°F'.format(f))
+c = float(input('Digite a temperatura em °c: '))
+f = float((9 * c) / 5) + 32
+print('A temperatura em fahrenheit: {0}°F'.format(f))
 
-# # Fatorial
+# Fatorial
 
-# c = f = n = 0
+c = f = n = 0
 
-# n = int(input("Digite um número para saber seu fatorial: "))
+n = int(input("Digite um número para saber seu fatorial: "))
 
-# if (n == 0):
-#     print(1)
-# else:
-#    f = n
-#    c = 1
-#    while (c < n):
-#         f = f*(n - c)
-#         c = c + 1
-# print(f)
+if (n == 0):
+    print(1)
+else:
+   f = n
+   c = 1
+   while (c < n):
+        f = f*(n - c)
+        c = c + 1
+print(f)
 
-# # Palíndromo
+# Palíndromo
 
-# pali = input('Digite um palavra: ').upper().replace(' ','')
+pali = input('Digite um palavra: ').upper().replace(' ','')
 
-# if pali == pali[:: -1]:
-#     print('A palavra é Palindroma','\n')
-#     print('Palavra Invertida: {}'.format(pali),'\n')
-# else:
-#     print('A palavra não é Palindroma','\n')
+if pali == pali[:: -1]:
+    print('A palavra é Palindroma','\n')
+    print('Palavra Invertida: {}'.format(pali),'\n')
+else:
+    print('A palavra não é Palindroma','\n')
 
-# print('Palavra Digitada: {} '.format(pali),'\n')
+print('Palavra Digitada: {} '.format(pali),'\n')
 
 # Contar Vogais
 
-# frase = input("Digite uma palavra/frase: ")
-# vogais = "aeiou"
+frase = input("Digite uma palavra/frase: ")
+vogais = "aeiou"
 
-# frase = frase.lower()
+frase = frase.lower()
 
-# total = 0
-# for vogal in vogais:
-#     total += frase.count(vogal)
+total = 0
+for vogal in vogais:
+    total += frase.count(vogal)
 
-# print("Total: ", total)
+print("Total: ", total)
 
 # Maior valor em Lista
 
-# notas = input("Digite uma sequência de números: ").split()
-# for n in range(0, len (notas)):
-#     notas[n] = int(notas[n])
-# print(notas[n])
+notas = input("Digite uma sequência de números: ").split()
+for n in range(0, len (notas)):
+    notas[n] = int(notas[n])
+print(notas[n])
 
-# maior = 0
-# for nota in notas:
-#     if nota > maior:
-#         maior = nota
-# print("O maior valor é: ", maior)
+maior = 0
+for nota in notas:
+    if nota > maior:
+        maior = nota
+print("O maior valor é: ", maior)
 
 # Ordem Crescente
 
-# n1 = int(input('Digite um número '))
-# n2 = int(input('Digite outro '))
-# n3 = int(input('Digite outro ')) 
-# n4 = int(input('Digite outro '))
-# n5 = int(input('Digite outro '))
+n1 = int(input('Digite um número '))
+n2 = int(input('Digite outro '))
+n3 = int(input('Digite outro ')) 
+n4 = int(input('Digite outro '))
+n5 = int(input('Digite outro '))
 
-# Lsta=[n1,n2,n3,n4,n5]
-# print('Essa é a lista em ordem crescente:',sorted(Lsta))
+Lsta=[n1,n2,n3,n4,n5]
+print('Essa é a lista em ordem crescente:',sorted(Lsta))
 
 # Números Primos
-# number = int(input("Digite um número: "))
-# if number > 1:
-#     for i in range(2, number):
-#         if number % i == 0:
-#             print(number, 'não é primo')
-#             break
-#     else:
-#         print(number, 'é primo')
+number = int(input("Digite um número: "))
+if number > 1:
+    for i in range(2, number):
+        if number % i == 0:
+            print(number, 'não é primo')
+            break
+    else:
+        print(number, 'é primo')
 
 # Juros
 
