cbcbb04e361261317edd79357050cab786c87958
mintysalad <uaupietra@gmail.com>
2024-12-03 12:26:19 +0000

    cotacoes

--- Python/cotacoes.py
+++ Python/cotacoes.py
@@ -18,13 +18,18 @@ def pegar_cotacoes():
 
 janela = Tk()
 janela.title("Cotação Atual de Moedas :D")
+janela.geometry("300x400")
+janela.configure(bg="#83c3de")
 texto = Label(janela, text="Clique no botão para ver as cotações de moedas")
-texto.grid(column=0, row=0, padx=10, pady=10)
+texto.configure(bg="#83c3de")
+texto.grid(column=1, row=0, padx=10, pady=10)
 
 botao = Button(janela, text="Buscar cotações", command=pegar_cotacoes)
+botao.configure(bg="grey", fg="#fffafa")
 
-botao.grid(column=0, row=1, padx=10, pady=10)
+botao.grid(column=1, row=1, padx=10, pady=10)
 textoR = Label(janela, text="")
-textoR.grid(column=0, row=2, padx=10, pady=10)
+textoR.grid(column=1, row=2, padx=10, pady=10)
+textoR.configure(bg="#83c3de")
 
 janela.mainloop()
