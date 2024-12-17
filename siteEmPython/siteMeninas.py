d05798a028f6b28eb717a6a33a1eac5c3ef0eec5
mintysalad <uaupietra@gmail.com>
2024-12-17 14:19:35 +0000

    siteMeninas.py

--- siteEmPython/siteMeninas.py
+++ siteEmPython/siteMeninas.py
@@ -1,17 +1,43 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+# Configurando o diretório dos templates e do app
+template_dir = os.path.abspath("./templates")
+app = Flask(__name__, template_folder=template_dir)
 # route -> meowmeow.com/
 # função -> o que vc quer exibir naquela página
 # template
 
+# Rota para servir a págia inicial
 @app.route("/")
 def homepage():
-    return "Esse é meu primeiro site :D (Salad)"
+    return render_template("index.html")
 
-@app.route("/contatos")
-def contatos():
-    return "<p>Contatos do Site: </p>Email: meowmeow@gmail.com <br>Fone: (051)989708786"
+# Rota para subpáginas do site
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/matricula")
+def matricula():
+    return render_template("matricula.html")
+
+# Rotas para servir arquivos estáticos na pasta "templates"
+@app.route("/img/<path:filename")
+def img_static(filename):
+    send_from_directory(os.path.join(template_dir, "img", filename))
+
+@app.route("/css/<path:filename")
+def css_static(filename):
+    send_from_directory(os.path.join(template_dir, "css", filename))
+
+@app.route("/js/<path:filename")
+def js_static(filename):
+    send_from_directory(os.path.join(template_dir, "js", filename))
+
+@app.route("/video/<path:filename")
+def video_static(filename):
+    send_from_directory(os.path.join(template_dir, "video", filename))
 
 # colocar site no ar
 if __name__ == "__main__":
---
+++ siteEmPython/siteMeninasCopia.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# route -> meowmeow.com/
+# função -> o que vc quer exibir naquela página
+# template
+
+@app.route("/")
+def homepage():
+    return "Esse é meu primeiro site :D (Salad)"
+
+@app.route("/contatos")
+def contatos():
+    return "<p>Contatos do Site: </p>Email: meowmeow@gmail.com <br>Fone: (051)989708786"
+
+# colocar site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
